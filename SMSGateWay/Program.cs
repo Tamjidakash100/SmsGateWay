526d7d7c0ad1279d9a21004093d5318e2034c9e9
tamjidakash100 <tamjidakash100@gmail.com>
2024-10-08 13:18:14 +0600

    Test simple SmsGateWay

---
+++ SMSGateWay/Program.cs
@@ -0,0 +1,11 @@
+﻿using SMSGateWay;
+
+string BaseApi = "https://api.sms.net.bd/sendsms";
+string apiKey = "Api-Key";
+string messageBody = "Message";
+string number2 = "Number That I want to send to";
+
+SendSms sendSms = new();
+
+var response = await sendSms.SendSmsAsync(BaseApi, apiKey, messageBody, number2);
+Console.WriteLine(response.ToString());
---
+++ SMSGateWay/SendSms.cs
@@ -0,0 +1,17 @@
+﻿namespace SMSGateWay;
+
+public class SendSms
+{
+    public async Task<bool> SendSmsAsync(string BaseApi, string apiKey, string messageBody, string number)
+    {
+        using HttpClient _httpClient = new HttpClient();
+        // Define the API endpoint
+        var apiUrl = $"{BaseApi}?api_key={apiKey}&msg={messageBody}&to={number}";
+
+        // Send a POST request with JSON data
+        HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+        // Check if the request was successful
+        return response.IsSuccessStatusCode;
+    }
+}
